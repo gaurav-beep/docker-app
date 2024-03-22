ad21fae6dcc64d3afa0dd98798e061901e18a358
Gaurav.Yadav <gauravyadavyadav3590@gmail.com>
2024-03-22 16:23:00 +0530

    added cross origin

--- src/main/java/com/docker/controller/TestController.java
+++ src/main/java/com/docker/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.docker.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TestController {
 @GetMapping("/test")
 public String test() {
