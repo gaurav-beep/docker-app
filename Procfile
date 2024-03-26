d063ac7a7c1de215df3fc9167a3ef4b632d3fdd8
Gaurav Yadav <gauravyadavyadav3590@gmail.com>
2024-03-26 12:53:33 +0530

    property update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/docker-app.jar
--- src/main/java/com/docker/controller/TestController.java
+++ src/main/java/com/docker/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.docker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TestController {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 @GetMapping("/test")
 public String test() {
 	return "test";
+}
+
+@GetMapping("/dbstatus")
+public String getDatabaseStatus() {
+    try {
+        jdbcTemplate.execute("SELECT 1");
+        return "Database connection is successful";
+    } catch (Exception e) {
+        return "Failed to connect to the database: " + e.getMessage();
+    }
 }
 }
